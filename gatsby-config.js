b219861115ca7c1cecd7b4d761653c06490bb195
Nathen Bowes <nathen.bowes@shopify.com>
2020-02-09 13:48:44 -0800

    added nav

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    menuLinks: [
+      {
+        name: `Products`,
+        link: `/products`
+      }
+    ],
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -34,6 +40,7 @@ module.exports = {
         accessToken: `a6e72da3a1161d69e5f460ad84c0638a`
       },
     },
+    `gatsby-plugin-emotion`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
